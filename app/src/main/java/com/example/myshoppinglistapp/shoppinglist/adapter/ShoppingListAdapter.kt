87eafee60509166a6d0b47326b7d9d77d72a6b36
marcin.brzeczek <Morris.150>
2020-07-19 17:48:48 +0200

    Renamed view holder.

--- app/src/main/java/com/example/myshoppinglistapp/shoppinglist/adapter/ShoppingListAdapter.kt
+++ app/src/main/java/com/example/myshoppinglistapp/shoppinglist/adapter/ShoppingListAdapter.kt
@@ -8,17 +8,17 @@ import com.example.myshoppinglistapp.R
 import com.example.myshoppinglistapp.shoppinglist.model.ShoppingList
 
 class ShoppingListAdapter(private val shoppingLists: List<ShoppingList>) :
-    RecyclerView.Adapter<ShoppingListViewHolder>() {
+    RecyclerView.Adapter<ShoppingRecyclerViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingListViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingRecyclerViewHolder {
         val title = LayoutInflater.from(parent.context)
             .inflate(R.layout.shopping_list_item, parent, false)
-        return ShoppingListViewHolder(title)
+        return ShoppingRecyclerViewHolder(title)
     }
 
     override fun getItemCount(): Int = shoppingLists.size
 
-    override fun onBindViewHolder(holder: ShoppingListViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ShoppingRecyclerViewHolder, position: Int) {
         (holder.itemView as TextView).text = shoppingLists[position].name
     }
 }
