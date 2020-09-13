d3399bd6516d8cbe664c83ccfb492d3be36d7d0d
marcin.brzeczek <Morris.150>
2020-09-13 13:07:49 +0200

    SL-04: Implemented navigation components.

---
+++ app/src/main/java/com/example/myshoppinglistapp/presentation/menu/ArchiveFragment.kt
@@ -0,0 +1,19 @@
+package com.example.myshoppinglistapp.presentation.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myshoppinglistapp.R
+
+class ArchiveFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_archive, container, false)
+    }
+}
